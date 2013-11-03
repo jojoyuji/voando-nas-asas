097fdc3e3bcfe1e097cf3326c936d1d71f559f2b
Jojo <jojoyuji@gmail.com>
2013-11-02 23:10:54 -0300

    INtegration with google maps

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,31 +23,31 @@ $(function(){
     var st = $(this).scrollTop();
     //top.css({ 'height' : ((90 - st <20)? 10 : 90-st) });
     //if (st> areas.header.h && st<areas.purpose.h){
-    //console.log(st);
-    //console.log("premero");
-    //$('.nav-bullets a').not(areas.header.e).removeClass('active');
-    //$(areas.header.e).addClass('active');
-    //}
-    //else if (st> areas.purpose.h && st< areas.prepare.h){
-    //$('.nav-bullets a').not(areas.purpose.e).removeClass('active');
-    //$(areas.purpose.e).addClass('active');
-    //}
-    //else if (st> areas.prepare.h){
-      //$('.nav-bullets a').not(areas.prepare.e).removeClass('active');
-    //$(areas.prepare.e).addClass('active');
-    //}
+      //console.log(st);
+      //console.log("premero");
+      //$('.nav-bullets a').not(areas.header.e).removeClass('active');
+      //$(areas.header.e).addClass('active');
+      //}
+      //else if (st> areas.purpose.h && st< areas.prepare.h){
+        //$('.nav-bullets a').not(areas.purpose.e).removeClass('active');
+        //$(areas.purpose.e).addClass('active');
+        //}
+        //else if (st> areas.prepare.h){
+          //$('.nav-bullets a').not(areas.prepare.e).removeClass('active');
+          //$(areas.prepare.e).addClass('active');
+          //}
 
-    //logo effect on scroll
-    if(st > 50 && !reduced) {
-      top.addClass('reduced').find('img').addClass('reduced');
-      reduced = true;
-    }
-    else if(st < 50 && reduced) {
-      top.removeClass('reduced').find('img').removeClass('reduced');
-      reduced = false;
-    }
-    //change opacity
-    //text_header.css({ 'opacity' : 1 - (st/400) });
+          //logo effect on scroll
+          if(st > 50 && !reduced) {
+            top.addClass('reduced').find('img').addClass('reduced');
+            reduced = true;
+          }
+          else if(st < 50 && reduced) {
+            top.removeClass('reduced').find('img').removeClass('reduced');
+            reduced = false;
+          }
+          //change opacity
+          //text_header.css({ 'opacity' : 1 - (st/400) });
   }
 
   $('a[href*=#]:not([href=#])').click(function() {
@@ -68,3 +68,5 @@ $(function(){
     }
   });
 });
+
+
