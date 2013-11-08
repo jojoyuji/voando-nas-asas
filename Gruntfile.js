68cce5ceee1f5c4cc3f6019f35feb232478d4269
Jojo <jojoyuji@gmail.com>
2013-11-08 09:20:21 -0300

    Update no mapa

--- Gruntfile.js
+++ Gruntfile.js
@@ -251,6 +251,7 @@ module.exports = function (grunt) {
                 '*.{ico,png,txt}',
                 '.htaccess',
                 'images/{,*/}*.{webp,gif}',
+                'files/{,*/}*',
                 'styles/fonts/{,*/}*.*'
               ]
             }]
